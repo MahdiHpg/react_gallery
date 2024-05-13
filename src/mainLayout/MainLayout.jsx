dbad4ea49a55952b8e6156a20489dfb0a3cec7dd
Mahdi heydarpour <hpgmahdi@gmail.com>
2024-05-13 23:17:30 +0330

    removed Unuseable folders and files

--- src/mainLayout/MainLayout.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/header/Header";
-import Footer from "../components/Footer";
-
-const MainLayout = () => {
-  return <></>;
-};
-export default MainLayout;
